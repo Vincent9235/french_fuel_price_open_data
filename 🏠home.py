1e441ccb68f5640e820313f13d631762789aac72
Vincent9235 <vincentf.laurens@orange.fr>
2023-06-08 11:07:51 +0200

    Why MEP exists ?

--- 🏠home.py
+++ 🏠home.py
@@ -148,7 +148,6 @@ for prix_element in root.iter('prix'):
                 prix_par_carburant_6_mois[nom_carburant] = [(date_maj, valeur_prix)]
                 
 # Average change in fuel prices over the past 30 days :
-
 # Create a Pandas DataFrame from price data
 df_prix = pd.DataFrame()
 for carburant, prix in prix_par_carburant.items():
@@ -161,9 +160,9 @@ df_prix['date'] = pd.to_datetime(df_prix['date'])
 
 # Group prices by date and fuel type
 df_agrege = df_prix.groupby([df_prix['date'].dt.date, 'carburant']).mean().reset_index()
-
+df_agrege.rename(columns={'date': 'date_aggregated'}, inplace=True)  # Rename the 'date' column
 # Create a graph showing the average change in fuel prices over the last 30 days
-fig = px.line(df_agrege, x='date', y='prix', color='carburant', title='Average change in fuel prices (last 30 days)')
+fig = px.line(df_agrege, x='date_aggregated', y='prix', color='carburant', title='Average change in fuel prices (last 30 days)')
 
 # Display pot 
 st.plotly_chart(fig)
